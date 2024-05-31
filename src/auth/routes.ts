ff579c7da5bf121e7887b9158e7d0cd38714e848
rikcav <henriquearau000@gmail.com>
2024-05-31 09:38:09 -0300

    feat: register route added

--- src/auth/routes.ts
+++ src/auth/routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import { authenticateController } from "./controller";
+import { authenticateController, register } from "./controller";
 
 export const authRoutes = (app: express.Application) => {
-  app.get("/auth/register", () => {});
+  app.get("/auth/register", register);
   app.get("/auth/login", authenticateController);
 };
