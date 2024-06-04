390af54207bf82790e59325485d957117ae1f9b1
rikcav <henriquearau000@gmail.com>
2024-06-03 21:03:28 -0300

    fix: update routes import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import { authRoutes } from "./auth/routes";
 
 const routes = require("./routes");
 const app = express();
@@ -14,7 +13,6 @@ routes(app);
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello World!");
 });
-app.use(authRoutes)
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
