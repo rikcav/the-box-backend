bb053668a12acaf9f09e3ad54318cb540128fcaf
rikcav <henriquearau000@gmail.com>
2024-06-03 21:05:15 -0300

    fix: fixed http method from get to post

--- src/auth/routes.ts
+++ src/auth/routes.ts
@@ -2,6 +2,6 @@ import express from "express";
 import { authenticateController, register } from "./controller";
 
 export const authRoutes = (app: express.Application) => {
-  app.get("/auth/register", register);
+  app.post("/auth/register", register);
   app.post("/auth/login", authenticateController);
 };
