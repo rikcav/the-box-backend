64678ef152154d6845500d82e0aa31423cb8820c
rikcav <henriquearau000@gmail.com>
2024-06-05 16:43:39 -0300

    feat: create new post route added

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { authRoutes } from "./auth/routes";
+import { postRoutes } from "./post/routes";
 
 module.exports = (app: express.Application) => {
   authRoutes(app);
+  postRoutes(app);
 };
