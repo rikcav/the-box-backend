59178bb55229e4f4c99a27b2dcfa78bbc06e5410
rikcav <henriquearau000@gmail.com>
2024-06-06 15:29:05 -0300

    feat: search routes added to post routes

--- src/post/routes.ts
+++ src/post/routes.ts
@@ -2,5 +2,7 @@ import express from "express";
 import * as postController from "./controller";
 
 export const postRoutes = (app: express.Application) => {
+  app.get("/post", postController.getPosts);
+  app.get("/post/:id", postController.getPost);
   app.post("/post", postController.createPost);
 };
