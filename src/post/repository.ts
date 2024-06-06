f009a7fad541cbd9ded5fa49bb11312d6e46b471
rikcav <henriquearau000@gmail.com>
2024-06-06 16:16:38 -0300

    feat: search posts by category

--- src/post/repository.ts
+++ src/post/repository.ts
@@ -1,7 +1,18 @@
 import { prisma } from "../prisma/service";
+import { PostCategoryEnum } from "@prisma/client";
 
 export const getPosts = async () => {
   const posts = await prisma.post.findMany();
+  return posts;
+};
+
+export const getPostsByCategory = async (category: PostCategoryEnum) => {
+  const posts = await prisma.post.findMany({
+    where: {
+      category,
+    },
+  });
+
   return posts;
 };
 
