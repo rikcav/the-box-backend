c67ff1806bd048b78b180dce1c439cda9be10652
Ronald <ronaldmacario13@gmail.com>
2024-06-05 21:45:20 -0300

    chore: Update authentication middleware to handle token validation and user blacklist tokens

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { verify } from "jsonwebtoken";
 import { env } from "../env";
+import { prisma } from "../prisma/service";
+import { compare } from "bcrypt";
 
 export const authentication = async (
     request: express.Request,
@@ -18,7 +20,30 @@ export const authentication = async (
     const [, token] = autorToken.split(" ");
 
     try {
-        request.body.userId = verify(token, env.SECRET_KEY).sub;
+        const decoded = verify(token, env.SECRET_KEY);
+        const userId = (decoded as any).sub;
+
+        const user = await prisma.user.findUnique({
+        where: { id: userId },
+        select: { blacklistTokens: true },
+        });
+
+        if (!user) {
+        return response.status(401).json({
+            message: "Invalid token!",
+        });
+        }
+
+        for (const blacklistedToken of user.blacklistTokens) {
+        const match = await compare(token, blacklistedToken);
+        if (match) {
+            return response.status(401).json({
+            message: "Token is invalid!",
+            });
+        }
+        }
+
+        request.body.userId = userId;
         return next();
     } catch (err) {
         return response.status(403).json({
