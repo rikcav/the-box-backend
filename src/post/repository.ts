b5dd46ca5945de64d7c9959cf16a2f48b39ff239
rikcav <henriquearau000@gmail.com>
2024-06-06 15:00:54 -0300

    feat: search methods for posts in repository

--- src/post/repository.ts
+++ src/post/repository.ts
@@ -1,5 +1,20 @@
 import { prisma } from "../prisma/service";
 
+export const getPosts = async () => {
+  const posts = await prisma.post.findMany();
+  return posts;
+};
+
+export const getPost = async (id: number) => {
+  const post = await prisma.post.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  return post;
+};
+
 export const createPost = async (data: any) => {
   const post = await prisma.post.create({
     data,
