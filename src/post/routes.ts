4382ece0f5812a31b3b85389b538b43f3492cd70
rikcav <henriquearau000@gmail.com>
2024-06-06 16:27:07 -0300

    feat: get posts by category route

--- src/post/routes.ts
+++ src/post/routes.ts
@@ -3,6 +3,7 @@ import * as postController from "./controller";
 
 export const postRoutes = (app: express.Application) => {
   app.get("/post", postController.getPosts);
+  app.get("/post/:category", postController.getPostsByCategory);
   app.get("/post/:id", postController.getPost);
   app.post("/post", postController.createPost);
 };
