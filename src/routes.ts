5f5a781f7cadac13255a0b512f1b6a1bd345d600
rikcav <henriquearau000@gmail.com>
2024-06-08 10:42:25 -0300

    feat: comment routes added

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { authRoutes } from "./auth/routes";
 import { postRoutes } from "./post/routes";
+import { commentRoutes } from "./comment/routes";
 
 module.exports = (app: express.Application) => {
   authRoutes(app);
   postRoutes(app);
+  commentRoutes(app);
 };
