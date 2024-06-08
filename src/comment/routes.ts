ef128d1c74f7188673b838cf1c7b527e0e5d3432
Ronald <ronaldmacario13@gmail.com>
2024-06-08 15:06:51 -0300

    feat: Add createNewComment endpoint to comment routes

--- src/comment/routes.ts
+++ src/comment/routes.ts
@@ -2,5 +2,6 @@ import express from "express";
 import * as commentController from "./controller";
 
 export const commentRoutes = (app: express.Application) => {
+  app.post("/comment", commentController.createNewComment);
   app.delete("/comment/:id", commentController.deleteById);
 };
