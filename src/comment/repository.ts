fa96af26a245d0a21e4286805d54308fcfe555c7
amadeusantos <amadeusantos31@gmail.com>
2024-06-08 18:24:06 -0300

    merger feat/create-comment-to-post

--- src/comment/repository.ts
+++ src/comment/repository.ts
@@ -1,5 +1,13 @@
 import { prisma } from "../prisma/service";
 
+export const createComment = async (data: any) => {
+  const comment = await prisma.comment.create({
+    data,
+  });
+
+  return comment;
+};
+
 export const deleteById = async (id: number) => {
   const comment = await prisma.comment.delete({
     where: {
--- src/comment/service.ts
+++ src/comment/service.ts
@@ -1,6 +1,25 @@
 import { NotFoundException } from "../errors/not-found-exception";
 import { prisma } from "../prisma/service";
 import * as commentRepository from "./repository";
+import { commentValidation } from "./validation";
+
+interface CreateCommentDto {
+  body: string;
+  user_id: number;
+  post_id: number;
+};
+
+export const createComment = async (dataCommnent: CreateCommentDto) => {
+  try {
+    const data = commentValidation.parse(dataCommnent);
+    const post = await commentRepository.createComment(data);
+
+    return post;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
 export const deleteById = async (id: number) => {
   try {
