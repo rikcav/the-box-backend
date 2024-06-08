fd44cf34cb489488ea186d7ccbb4501edf98be84
rikcav <henriquearau000@gmail.com>
2024-06-08 10:40:58 -0300

    feat: delete comment method in service

---
+++ src/comment/service.ts
@@ -0,0 +1,11 @@
+import * as commentRepository from "./repository";
+
+export const deleteById = async (id: number) => {
+  try {
+    const comment = await commentRepository.deleteById(id);
+    return comment;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
