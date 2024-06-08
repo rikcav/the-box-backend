9ef2023305f235f794fb17f4e197a61e16492439
Ronald <ronaldmacario13@gmail.com>
2024-06-08 15:06:15 -0300

    feat: add createComment method to comment service

--- src/comment/service.ts
+++ src/comment/service.ts
@@ -1,10 +1,23 @@
 import * as commentRepository from "./repository";
+import { commentValidation } from "./validation";
+
 interface CreateCommentDto {
   body: string;
   user_id: number;
   post_id: number;
 };
 
+export const createComment = async (dataCommnent: CreateCommentDto) => {
+  try {
+    const data = commentValidation.parse(dataCommnent);
+    const post = await commentRepository.createComment(data);
+
+    return post;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
 export const deleteById = async (id: number) => {
   try {
