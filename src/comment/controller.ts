d69693caa6c55f98c314c1ec86ee46a6302aa927
Ronald <ronaldmacario13@gmail.com>
2024-06-08 15:06:34 -0300

    feat: add createNewComment endpoint to comment controller

--- src/comment/controller.ts
+++ src/comment/controller.ts
@@ -1,5 +1,26 @@
 import express from "express";
 import * as commentService from "./service";
+import { ZodError } from "zod";
+
+export const createNewComment = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const createCommentDTO = req.body;
+    const comment = await commentService.createComment(createCommentDTO);
+
+    res.status(200).send({ message: "Commented!", comment })
+  } catch (error) {
+    console.log(error);
+
+    if (error instanceof ZodError) {
+      res.status(400).send({ mensage: "Validation failed.", errors: error});
+    } else {
+      res.status(400).send({ message: "Could not create a comment.", errors: error});
+    }
+  }
+};
 
 export const deleteById = async (
   req: express.Request,
