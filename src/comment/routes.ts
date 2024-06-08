2edd43b76bad08a12f71e8035f0ff2b7480b6c3e
Ronald <ronaldmacario13@gmail.com>
2024-06-08 16:41:23 -0300

    Implement updateById endpoint in comment routes

--- src/comment/routes.ts
+++ src/comment/routes.ts
@@ -4,4 +4,5 @@ import * as commentController from "./controller";
 export const commentRoutes = (app: express.Application) => {
   app.post("/comment", commentController.createNewComment);
   app.delete("/comment/:id", commentController.deleteById);
+  app.put("/comment/:id", commentController.updateById);
 };
