319a50639361944aceb45d63521a32d24d117a48
rikcav <henriquearau000@gmail.com>
2024-06-09 09:11:17 -0300

    feat: lab routes added

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { authRoutes } from "./auth/routes";
 import { postRoutes } from "./post/routes";
 import { commentRoutes } from "./comment/routes";
+import { labRoutes } from "./lab/routes";
 
 module.exports = (app: express.Application) => {
   authRoutes(app);
   postRoutes(app);
   commentRoutes(app);
+  labRoutes(app);
 };
