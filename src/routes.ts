f9d17b1cd0421ac99677f4fdb465b9fa6325d3bd
rikcav <henriquearau000@gmail.com>
2024-06-11 16:01:27 -0300

    feat: user routes added

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { authRoutes } from "./auth/routes";
 import { postRoutes } from "./post/routes";
 import { commentRoutes } from "./comment/routes";
+import { userRoutes } from "./user/routes";
 
 module.exports = (app: express.Application) => {
   authRoutes(app);
   postRoutes(app);
   commentRoutes(app);
+  userRoutes(app);
 };
