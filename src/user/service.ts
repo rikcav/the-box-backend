3a52b40dea8fe7274edad8e119078923597e71ed
rikcav <henriquearau000@gmail.com>
2024-06-14 17:04:14 -0300

    fix(user): added validation for updating user

--- src/user/service.ts
+++ src/user/service.ts
@@ -4,7 +4,7 @@ import {
   updateUser as updateUserRepository,
   deleteById as deleteByIdRepository,
 } from "./repository";
-import { userValidation } from "./validation";
+import { updateUserValidation, userValidation } from "./validation";
 
 export const getUserById = async (userId: number) => {
   try {
@@ -34,11 +34,64 @@ export const createUser = async (userData: any) => {
   }
 };
 
-export const updateUser = async (id: number, userData: any) => {
+interface UpdateData {
+  name: string;
+  email: string;
+  phone: string;
+}
+
+const deepEqual = (obj1: any, obj2: any): boolean => {
+  if (obj1 === obj2) {
+    return true;
+  }
+
+  if (
+    typeof obj1 !== "object" ||
+    obj1 === null ||
+    typeof obj2 !== "object" ||
+    obj2 === null
+  ) {
+    return false;
+  }
+
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+export const updateUser = async (id: number, updateData: UpdateData) => {
   try {
-    const data = userValidation.parse(userData);
-    const user = updateUserRepository(id, data);
-    return user;
+    const data = updateUserValidation.parse(updateData);
+
+    if (deepEqual(data, updateData)) {
+      const storedUser = await getByIdRepository(id);
+
+      const userData = {
+        name: data?.name,
+        email: data?.email,
+        phone: data?.phone,
+        profile: storedUser.profile,
+        password: storedUser.password,
+      };
+
+      const validatedUserData = userValidation.parse(userData);
+      const user = updateUserRepository(id, validatedUserData);
+
+      return user;
+    }
+
+    throw "The input data is not valid";
   } catch (error) {
     console.log(error);
     throw error;
