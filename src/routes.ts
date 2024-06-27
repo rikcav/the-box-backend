c72755f8bf0b981a372ee1352db5543be4b9cf1a
rikcav <henriquearau000@gmail.com>
2024-06-27 18:36:19 -0300

    feat: event and event schedule routes registered

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,8 @@ import { postRoutes } from "./post/routes";
 import { commentRoutes } from "./comment/routes";
 import { userRoutes } from "./user/routes";
 import { scheduleRoutes } from "./schedule/routes";
+import { eventRoutes } from "./event/routes";
+import { eventScheduleRoutes } from "./eventSchedule/routes";
 
 module.exports = (app: express.Application) => {
   authRoutes(app);
@@ -11,4 +13,6 @@ module.exports = (app: express.Application) => {
   commentRoutes(app);
   userRoutes(app);
   scheduleRoutes(app);
+  eventRoutes(app);
+  eventScheduleRoutes(app);
 };
