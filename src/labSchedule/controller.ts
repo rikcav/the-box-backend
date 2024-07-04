eaf7cc5f3ed8567b15f5c9b704aec1a3dde351cf
Amadeu Santos <amadeusantos31@gmail.com>
2024-07-03 23:11:14 -0300

    Revert "Feat/register lab schedules [BK014]"

--- src/labSchedule/controller.ts
+++ src/labSchedule/controller.ts
@@ -1,69 +1,35 @@
-import express from "express";
-import * as service from "./service";
+import express  from "express"
+import * as labScheduleService from './service';
 import { ZodError } from "zod";
 
-export const getAllLabSchedules = async (
-  req: express.Request,
-  res: express.Response,
-) => {
+export const getAllLabs = async (req: express.Request, res: express.Response) => {
   try {
-    const labs = await service.getAll();
+    const labs = await labScheduleService.getAll();
     res.status(200).send(labs);
   } catch (error) {
     res.status(400).send({ message: "Could not get labs. ", error: error });
   }
 };
 
-export const getLabScheduleById = async (
-  req: express.Request,
-  res: express.Response,
-) => {
+export const getLabScheduleById = async (req: express.Request, res: express.Response) => {
   try {
     const id = parseInt(req.params.id);
-    const labSchedule = await service.get(id);
+    const labSchedule = await labScheduleService.get(id);
     res.status(200).json(labSchedule);
-  } catch (error) {
+} catch (error) {
     res.status(404).json({ message: "Lab Schedule not found" });
-  }
-};
-
-export const create = async (req: express.Request, res: express.Response) => {
-  try {
-    const labSchedule = await service.create(req.body);
-    res.status(201).send(labSchedule);
-  } catch (error) {
-    if (error instanceof ZodError) {
-      res.status(400).send({ message: "Validation error: ", error });
-    }
-    res.status(400).send({ message: "Could not create lab schedule", error });
   }
 };
 
 export const update = async (req: express.Request, res: express.Response) => {
   try {
-    const labSchedule = await service.update(parseInt(req.params.id), req.body);
-    console.log("Lab schedule updated: ", labSchedule);
+    const lab = await labScheduleService.update(parseInt(req.params.id), req.body);
+    console.log("Lab updated: ", lab);
     res.status(204).send();
   } catch (error) {
     if (error instanceof ZodError) {
-      res
-        .status(400)
-        .send({ message: "Validation error: ", error: error.errors });
+      res.status(400).send({ message: "Validation error: ", error: error.errors });
     }
-    res
-      .status(400)
-      .send({ message: "Could not update lab schedule: ", error: error });
-  }
-};
-
-export const remove = async (req: express.Request, res: express.Response) => {
-  try {
-    const id = parseInt(req.params.id);
-    const userId = parseInt(req.params.userid);
-    const labSchedule = await service.deleteById(id, userId);
-    console.log("Lab schedule deleted: ", labSchedule);
-    res.status(204).send();
-  } catch (error) {
-    res.status(400).send({ message: "Could not delete lab schedule", error });
+    res.status(400).send({ message: "Could not update lab: ", error: error });
   }
 };
