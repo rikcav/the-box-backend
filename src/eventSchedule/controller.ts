ca6a2d3459eb23cb0a7778ecad4bb8153e90177a
Henrique Cavalcante <henriquearau000@gmail>
2024-07-08 12:19:20 -0300

    feat(eventSchedule): fixed double response bug

--- src/eventSchedule/controller.ts
+++ src/eventSchedule/controller.ts
@@ -7,9 +7,7 @@ export const getAll = async (req: express.Request, res: express.Response) => {
     const event_schedules = await service.getAll();
     res.status(200).send(event_schedules);
   } catch (error) {
-    res
-      .status(400)
-      .send({ message: `Could not get event schedules`, error: error });
+    res.status(400).send({ message: `Could not get event schedules`, error });
   }
 };
 
@@ -23,10 +21,9 @@ export const getByScheduleId = async (
     );
     res.status(200).send(event_schedules);
   } catch (error) {
-    res.status(404).send({
-      message: `Could not get event schedules by schedule id`,
-      error: error,
-    });
+    res
+      .status(404)
+      .send({ message: `Could not get event schedules by schedule id`, error });
   }
 };
 
@@ -35,9 +32,7 @@ export const get = async (req: express.Request, res: express.Response) => {
     const event_schedule = await service.get(parseInt(req.params.id));
     res.status(200).send(event_schedule);
   } catch (error) {
-    res
-      .status(404)
-      .send({ message: `Could not get event schedule`, error: error });
+    res.status(404).send({ message: `Could not get event schedule`, error });
   }
 };
 
@@ -47,11 +42,12 @@ export const create = async (req: express.Request, res: express.Response) => {
     res.status(201).send(event_schedule);
   } catch (error) {
     if (error instanceof ZodError) {
-      res.status(400).send({ message: "Validation error", error: error });
+      res.status(400).send({ message: "Validation error", error });
+    } else {
+      res
+        .status(400)
+        .send({ message: `Could not create event schedule`, error });
     }
-    res
-      .status(400)
-      .send({ message: `Could not create event schedule`, error: error });
   }
 };
 
@@ -63,11 +59,12 @@ export const update = async (req: express.Request, res: express.Response) => {
     res.status(204).send();
   } catch (error) {
     if (error instanceof ZodError) {
-      res.status(400).send({ message: "Validation error", error: error });
+      res.status(400).send({ message: "Validation error", error });
+    } else {
+      res
+        .status(400)
+        .send({ message: `Could not create event schedule`, error });
     }
-    res
-      .status(400)
-      .send({ message: `Could not create event schedule`, error: error });
   }
 };
 
@@ -77,8 +74,6 @@ export const remove = async (req: express.Request, res: express.Response) => {
     console.log(event_schedule);
     res.status(204).send();
   } catch (error) {
-    res
-      .status(404)
-      .send({ message: `Could not get event schedule`, error: error });
+    res.status(404).send({ message: `Could not get event schedule`, error });
   }
 };
