f222fa41fd4c462e2bb1a8aa7e9acd76f1e869e0
Henrique Cavalcante <henriquearau000@gmail>
2024-07-08 10:20:42 -0300

    feat(labSchedule): added create and remove routes

--- src/labSchedule/routes.ts
+++ src/labSchedule/routes.ts
@@ -4,5 +4,7 @@ import * as controller from "./controller";
 export const labSchedulesRoutes = (app: express.Application) => {
   app.get("/labschedules", controller.getAllLabs);
   app.get("/labschedules/:id", controller.getLabScheduleById);
+  app.post("/labschedules", controller.create);
   app.put("/labschedules/:id", controller.update);
+  app.delete("/labschedules", controller.update);
 };
