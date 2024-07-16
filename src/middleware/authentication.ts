882089d48d963997d4d5768981aaf02f5b5585de
Jamuelton <jamueltonangelim@gmail.com>
2024-07-16 14:08:00 -0300

    chore: security commit

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -5,49 +5,49 @@ import { prisma } from "../prisma/service";
 import { compare } from "bcrypt";
 
 export const authentication = async (
-    request: express.Request,
-    response: express.Response,
-    next: express.NextFunction
-  ) => {
-    const autorToken = request.headers.authorization;
+  request: express.Request,
+  response: express.Response,
+  next: express.NextFunction
+) => {
+  const autorToken = request.headers.authorization;
 
-    if (!autorToken) {
-        return response.status(401).json({
-            message: "Missing token!"
-        });
-    }
-
-    const [, token] = autorToken.split(" ");
-
-    try {
-        const decoded = verify(token, env.SECRET_KEY);
-        const userId = (decoded as any).sub;
-
-        const user = await prisma.user.findUnique({
-        where: { id: userId },
-        select: { blacklistTokens: true },
-        });
-
-        if (!user) {
-        return response.status(401).json({
-            message: "Invalid token!",
-        });
-        }
-
-        for (const blacklistedToken of user.blacklistTokens) {
-        const match = await compare(token, blacklistedToken);
-        if (match) {
-            return response.status(401).json({
-            message: "Token is invalid!",
-            });
-        }
-        }
-
-        request.body.userId = userId;
-        return next();
-    } catch (err) {
-        return response.status(403).json({
-            message: "Invalid token!"
-        });
-    }
+  if (!autorToken) {
+    return response.status(401).json({
+      message: "Missing token!",
+    });
   }
+
+  const [, token] = autorToken.split(" ");
+
+  try {
+    const decoded = verify(token, env.SECRET_KEY);
+    const userId = (decoded as any).sub;
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: { blacklistTokens: true },
+    });
+
+    if (!user) {
+      return response.status(401).json({
+        message: "Invalid token!",
+      });
+    }
+
+    for (const blacklistedToken of user.blacklistTokens) {
+      const match = await compare(token, blacklistedToken);
+      if (match) {
+        return response.status(401).json({
+          message: "Token is invalid!",
+        });
+      }
+    }
+
+    request.body.userId = userId;
+    return next();
+  } catch (err) {
+    return response.status(403).json({
+      message: "Invalid token!",
+    });
+  }
+};
