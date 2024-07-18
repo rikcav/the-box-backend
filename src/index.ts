4be50b5e3e9f6662f5b6b96fb176134fcd3fa42c
Ronald <ronaldmacario13@gmail.com>
2024-07-18 05:37:53 -0300

    feat: Enable HTTPS with SSL certificate and key files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import https from 'https';
+import fs from 'fs';
 
 const routes = require("./routes");
 const app = express();
@@ -14,6 +16,15 @@ app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+const httpsOptions = {
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./cert.pem')
+};
+
+// app.listen(port, () => {
+//   console.log(`App listening at http://localhost:${port}`);
+// });
+
+https.createServer(httpsOptions, app).listen(port, () => {
+  console.log(`App listening at https://localhost:${port}`);
 });
